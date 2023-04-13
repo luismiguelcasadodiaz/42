3a44cf8431a0fa2b2b3aae6d3c4fd26303d19a30
luismiguelcasadodiaz <luismiguelcasadodiaz@gmail.com>
2023-04-13 11:52:29 +0200

    secodn kata

---
+++ cyber/python_0/ex05/kata01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+kata = {
+        'Python': 'Guido van Rossum',
+        'Ruby': 'Yukihiro Matsumoto',
+        'PHP': 'Rasmus Lerdorf',
+        }
+for k,v in kata.items():
+    print("{} was created bu {}".format(k,v))
