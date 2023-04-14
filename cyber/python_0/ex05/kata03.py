ce049fe273d70a4b22145d6639884beb6ec3deaf
luismiguelcasadodiaz <luismiguelcasadodiaz@gmail.com>
2023-04-14 11:41:45 +0200

    Kata 03 Error Handling

--- cyber/python_0/ex05/kata03.py
+++ cyber/python_0/ex05/kata03.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 kata = "The right format"
-print("{:->42}".format(kata), end="")
+kata = ""
+if len(kata) < 43:
+    print("{:->42}".format(kata), end="")
+else:
+    print(f"Text '{kata}' is too long here")
