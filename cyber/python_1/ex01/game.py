08de9f22cef813c46b5fbf855171c668128590c6
luismiguelcasadodiaz <luismiguelcasadodiaz@gmail.com>
2023-04-19 10:09:38 +0200

    added docstrings

--- cyber/python_1/ex01/game.py
+++ cyber/python_1/ex01/game.py
@@ -1,4 +1,4 @@
-#!\usr\bin\python3
+#!/usr/bin/python3
 
 
 """
@@ -11,38 +11,44 @@ Create a GotCharacter class and initialize it with the following attributes:
 • is_alive (by default is True).
 
 """
+
+
 class GotCharacter:
-    def __init__(self, first_name ="No Name given", is_alive = True):
+    """
+        A Class representing a Game's character.
+    """
+    def __init__(self, first_name="No Name given", is_alive=True):
         self.first_name = first_name
         self.is_alive = is_alive
-    
+
     def __str__(self):
-                
+
         if self.is_alive:
             return f"I am '{self.first_name}' and i am alive"
-        else:        
+        else:
             return f"I an the soul of '{self.first_name}' that passed away"
-    
+
     def __repr__(self):
         return f"GotCharacter('{self.first_name}', {self.is_alive})"
 
+
 """
-
-
-Pick up a GoT House (e.g., Stark, Lannister...) and create a child class that inherits
-from GotCharacter and define the following attributes:
+Pick up a GoT House (e.g., Stark, Lannister...) and create a child class
+that inherits from GotCharacter and define the following attributes:
 • family_name (by default should be the same as the Class)
-• house_words (e.g., the House words for the Stark House is: "Winter is Coming")
-
+• house_words (e.g., the House words for the Stark House is:
+    "Winter is Coming")
 """
+
+
 class Lannister(GotCharacter):
     """
         A Class representing the Lannister family.
         or when bad things happen to good people.
     """
-    def __init__(self, first_name ="No Name given", is_alive = True):
-        super().__init__(first_name = first_name, is_alive = True)
-        self.famuly_name ="Lannister"
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=True)
+        self.famivacialy_name = "Lannister"
         self.house_words = "Hear Me Roar"
 
     """
@@ -75,7 +81,7 @@ if __name__ == "__main__":
     pepe = GotCharacter("LUis")
     print("pepe", pepe)
     pepo = eval(repr(pepe))
-    print("Pepo",pepo)
+    print("Pepo", pepo)
     arya = Lannister("Arya")
     print(arya)
     arya.print_house_words()
@@ -85,3 +91,5 @@ if __name__ == "__main__":
     arya.die()
     print(arya.__doc__)
     print(arya)
+    pepe = GotCharacter("")
+    print("pepe", pepe)
