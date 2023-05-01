821a0b6ddac1a80cd51595e3491adf26f8479d0b
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-05-01 12:37:08 +0200

    pycodestyled Scorpion prints EXIT and other metadata but JPEGThumbnail

--- cyber/arachnida/scorpion.py
+++ cyber/arachnida/scorpion.py
@@ -1,29 +1,61 @@
 #!/usr/bin/python3
 
 """
-Le deuxième programme scorpion recevra des fichiers d’images en tant que 
+Le deuxième programme scorpion recevra des fichiers d’images en tant que
 paramètres et sera capable de les analyser pour en extraire les métadonnées
-EXIF et autres, en les affichant à l’écran. 
+EXIF et autres, en les affichant à l’écran.
 
-Le programme sera au moins compatible avec les mêmes extensions que celles 
+Le programme sera au moins compatible avec les mêmes extensions que celles
 gérées par spider. Il affichera les attributs de base tels que la date de
 création, ainsi que les données EXIF. Le format de sortie dépend de vous.
 
 
 ./scorpion FILE1 [FILE2 ...]
 """
-import  exif
-
+import argparse
+import exifread
+import pprint
 import sys
+import os
+
+ALLOWED_IMG_EXTE = tuple([".jpg", ".JPG", ".jpeg", ".JPEG", ".png",
+                          ".PNG", ".gif", ".GIF", ".bmp", ".BMP"])
+
+
+def options():
+    parser = argparse.ArgumentParser(description="Scorpion Reads
+                                     image metadata")
+    parser.add_argument("file", help="Input image file.", nargs='+')
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == '__main__':
-    numarg = len(sys.argv)
-    if numarg == 1:
-        print(f"Usage is ./scorpion FILE1 [FILE2 ...]")
-    else:
-        for file in sys.argv[1:]:
-            with open(file, 'rb') as image_file:
-                my_image = exifImage(image_file)
-                if my_image.has_exif:
-                    for list in my_image.list_all():
-                        print(list)
+    args = options()
+    print(args)
+
+    cwd = os.getcwd()
+
+    for file in args.file:
+        extension = file[file.rfind("."):]
+        # print("==>",file, extension)
+        if extension not in ALLOWED_IMG_EXTE:
+            print(f"File format {extension} not supported")
+            print(f"I am ready only for
+                  {[x for idx, x in enumerate(ALLOWED_IMG_EXTE)
+                   if (idx % 2) == 0]}")
+            exit()
+        scorpiopath = os.path.join(cwd, file)
+        with open(scorpiopath, 'rb') as image_file:
+            my_image = exifread.process_file(image_file)
+            tags = my_image.keys()
+            if len(tags) == 0:  # this imaga has not EXIF data
+                print(f"No metadata in {scorpiopath}")
+            else:
+                print()
+                print(f"Metadata  from {scorpiopath}")
+                print("==============", "=" * len(scorpiopath))
+                for tag in my_image.keys():
+                    if tag != "JPEGThumbnail":
+                        print(f"{tag}:{my_image[tag]}")
+                print("==============", "=" * len(scorpiopath))
